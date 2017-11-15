bfe3ee59c0c5eb417299c4ef2917dfe857b89e64
Chris Martin <ch.martin@gmail.com>
2017-11-14 23:39:08 -0500

    gtetrinet: init at 0.7.11

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17923,6 +17923,8 @@ with pkgs;
 
   gogui = callPackage ../games/gogui {};
 
+  gtetrinet = callPackage ../games/gtetrinet { };
+
   gtypist = callPackage ../games/gtypist { };
 
   gzdoom = callPackage ../games/gzdoom { };
